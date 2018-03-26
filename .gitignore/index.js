95356858aac0495198a4716d78d888de985bdb1e
dragophe <37804561+dragophe@users.noreply.github.com>
2018-03-26 16:14:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -17,7 +17,7 @@ bot.on('message' , function (message) {
 
 })
 
-bot.login('NDI3NzY4ODg2MzMxNDQxMTY1.DZqRXA.avbj_SwPIWBtlvf8lP4jF7N39UI')
+bot.login('NDI3ODMyNjQ0NDc5MzUyODU0.DZqRuQ.p-zgik9ktWyF2A2Mc4gJzJFgdK8')
 
 //https://discordapp.com/oauth2/authorize?client_id=422689053184622593&scope=bot&permissions=3148800
 
